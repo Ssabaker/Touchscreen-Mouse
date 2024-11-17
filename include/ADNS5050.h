614bd857c56048d8984e287a27a578f310c41990
Sam Baker <bakerslhs@gmail.com>
2024-11-17 18:00:05 -0500

    Add files via upload

---
+++ include/ADNS5050.h
@@ -0,0 +1,61 @@
+/*
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+// Code from: https://github.com/joshuajnoble/ArduinoADNS5050/tree/master 
+
+#ifndef _ADNS5050
+#define _ADNS5050
+
+#include <inttypes.h>
+#include <Arduino.h>
+
+#undef abs
+#undef round
+
+#define NUM_PIXELS 360
+
+class ADNS5050
+{
+  private:
+	
+	int sclkPin, sdioPin, selectPin, resetPin;
+	
+  public:
+	
+	ADNS5050(int sclk, int sdio, int _select, int _reset);
+	int dx();
+	int dy();
+	
+	unsigned char surfaceQuality();
+	bool motion();
+
+	void sync();
+        
+	unsigned char ADNS_read(unsigned char addr);
+	void ADNS_write(unsigned char addr, unsigned char data);
+	
+	// NB this is not a particularly fast operation
+	// don't call it more than 20 times a second
+	void pixelGrab(unsigned char* fill);
+        unsigned char ADNS_readPix(unsigned char addr);
+	//void setResolution(int res);
+	
+};
+
+
+
+#endif
